40c9c6f73bc02a39f3b92ad4ee16c6871adeec0b
mboini <123262514+mboini@users.noreply.github.com>
2023-02-23 05:55:27 +0530

    Update ex_4_1.py

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -14,15 +14,8 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def num_shutdowns(logfile):
-    with open(logfile) as file:
-        lines = file.readlines()
-    count = 0
-    for i in range(len(lines)-1):
-        if contains_shutdown(lines[i], lines[i+1]):
-            count += 1
-    return count
-        
-
+    shutdown_events = get_shutdown_events(logfile)
+    return len(shutdown_events) // 2
 
 
 
